a0fc43a756dc71b23fd133b0773b6f571b13b09b
Inki <62001193+Inquipatia@users.noreply.github.com>
2026-09-11 10:41:31 -0300

    cambio de número spline

--- app/components/scenes/floating-social-orb.tsx
+++ app/components/scenes/floating-social-orb.tsx
@@ -57,7 +57,7 @@ type DanceStep = {
 };
 
 const SPLINE_SCENE_URL =
-  "https://prod.spline.design/rWCCsSXcaU52lNsR/scene.splinecode";
+  "prod.spline.design/rWCCsSXcaU52lNsR/scene.splinecode";
 
 const HOVER_SOUND_URL = "/sounds/openningbbpop.mp3";
 const BOUNCE_SOUND_URL = "/sounds/jump2_join.mp3";
